54e59a743b69ac1025ba014dbcdf175f6b1a9c84
nav-aiditalhub <nav@aidigitalhub.com>
2023-05-04 10:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/capabilities")
+def hello_world():
+    return render_template("capabilities.html")
